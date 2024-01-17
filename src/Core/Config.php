f34ec626cde5a3194f3805ee92a864ee532efd08
Remigiusz Nowicki <49556258+dotremilie@users.noreply.github.com>
2024-01-18 00:50:56 +0100

    Update Config.php

--- src/Core/Config.php
+++ src/Core/Config.php
@@ -4,8 +4,8 @@ namespace Wspomagacz\Core;
 
 class Config {
     const DB_HOST = 'localhost';
-    const DB_NAME = 'remizhob_wspomagacz';
-    const DB_USER = 'remizhob_wspomagacz';
+    const DB_NAME = 'remihzob_wspomagacz';
+    const DB_USER = 'remihzob_wspomagacz';
     const DB_PASSWORD = 'O;d4yL6;h)X*';
 }
 
