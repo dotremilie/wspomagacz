061d6593165debc35aebb4beefd652c4141ca9bb
Remigiusz Nowicki <49556258+dotremilie@users.noreply.github.com>
2024-01-17 04:11:44 +0100

    Revert "test for hosting"
    This reverts commit fbb5d9784f6778e654c297169a3e8634194c6f13.

--- server/public/index.php
+++ server/public/index.php
@@ -9,6 +9,4 @@ $router = new Router();
 
 $router->addRoute('GET', '/api/version', VersionController::class, 'index');
 
-echo $_SERVER['REQUEST_URI'];
-
 $router->dispatch($_SERVER['REQUEST_METHOD'], parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
