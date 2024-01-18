6721a89dd14eca3724e36bf0b5c18ae00416e556
Remigiusz Nowicki <49556258+dotremilie@users.noreply.github.com>
2024-01-18 03:21:25 +0100

    read Exercises from $data

--- src/View/Exercises/index.php
+++ src/View/Exercises/index.php
@@ -1,18 +1,30 @@
 <!doctype html>
 <html lang="en">
-<?php require_once __DIR__ . "/../../../templates/head.php"; ?>
-<?php require_once __DIR__ . "/../../../templates/header.php"; ?>
+<?php
+use Wspomagacz\Model\Equipment;
+use Wspomagacz\Model\Exercise;
+use Wspomagacz\Model\Muscle;
+
+require_once __DIR__ . "/../../../templates/head.php";
+require_once __DIR__ . "/../../../templates/header.php";
+?>
 <body class="bg-gray-100 dark:bg-slate-900 dark:text-white text-slate-800">
 <section class="mx-auto p-4">
-    <h2 class="text-xl font-bold mb-4">Kategoria Ćwiczeń</h2>
-    <div class="w-full flex flex-col justify-center items-center">
-        <a href="" class="w-full p-6 dark:bg-slate-800 rounded-xl flex justify-between items-center">
+    <div class="w-full flex flex-col gap-4 justify-center items-center">
+        <?php
+        /** @var array $data */
+        /** @var Exercise $exercise */
+        /** @var Equipment $equipment */
+        /** @var Muscle $muscle */
+        foreach ($data as $exercise): ?>
+        <a id="exercise-<?= $exercise->getId() ?>" href="" class="w-full p-6 dark:bg-slate-800 rounded-xl flex justify-between items-center">
             <div>
-                <p class="text-lg font-bold">Pompki</p>
-                <p class="text-gray-400">123 | 123 | 123</p>
+                <div class="text-lg font-bold"><?= $exercise->getName() ?></div>
+                <div class="text-gray-400"><?php foreach ($exercise->getEquipmentUsed() as $equipment) echo $equipment->getName() ?> | <?php foreach ($exercise->getMusclesUsed() as $muscle) echo $muscle->getName() ?></div>
             </div>
             <i data-feather="chevron-right" class="text-gray-400"></i>
         </a>
+        <?php endforeach; ?>
     </div>
 </section>
 <?php require_once __DIR__ . "/../../../templates/footer.php"; ?>
