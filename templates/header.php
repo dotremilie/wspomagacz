e951502cfaabe8bf11f9dac887088454ef853d92
Remigiusz Nowicki <49556258+dotremilie@users.noreply.github.com>
2024-01-18 03:20:49 +0100

    font size and color tweak for header text

--- templates/header.php
+++ templates/header.php
@@ -1,5 +1,5 @@
-<nav class="dark:bg-slate-900 bg-white p-4 dark:text-white text-slate-800">
+<nav class="dark:bg-slate-900 p-3 dark:text-slate-100 text-slate-800">
     <div class="w-full text-center">
-        <h1 class="text-2xl font-bold italic">WSPOMAGACZ</h1>
+        <h1 class="text-3xl font-bold italic">WSPOMAGACZ</h1>
     </div>
 </nav>
