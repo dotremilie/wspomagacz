b26f8ea99fcd5b26ae98aeeb3ded060ec9d6e76e
Remigiusz Nowicki <49556258+dotremilie@users.noreply.github.com>
2024-01-19 00:41:54 +0100

    Remove for now

--- src/View/Training/start.php
+++
@@ -1,6 +0,0 @@
-<!doctype html>
-<html lang="en">
-<?php require_once __DIR__ . "/../../../templates/head.php"; ?>
-<?php require_once __DIR__ . "/../../../templates/header.php"; ?>
-<?php require_once __DIR__ . "/../../../templates/footer.php"; ?>
-</html>
