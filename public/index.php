c6ce900ad3a65b5ec3c0d1589cb518e28d7e4194
Remigiusz Nowicki <49556258+dotremilie@users.noreply.github.com>
2024-01-18 17:40:32 +0100

    Update Routes

--- public/index.php
+++ public/index.php
@@ -4,6 +4,9 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use Wspomagacz\Controller\ExercisesController;
 use Wspomagacz\Controller\HomeController;
+use Wspomagacz\Controller\LoginController;
+use Wspomagacz\Controller\ProfileController;
+use Wspomagacz\Controller\SignupController;
 use Wspomagacz\Controller\StartupController;
 use Wspomagacz\Controller\RankingController;
 use Wspomagacz\Controller\TrainingController;
@@ -14,22 +17,18 @@ $router = new Router();
 $router->addRoute('GET', '/', HomeController::class, 'index');
 
 $router->addRoute('GET', '/startup', StartupController::class, 'index');
-$router->addRoute('GET', '/startup/login', StartupController::class, 'login');
-$router->addRoute('GET', '/startup/login', StartupController::class, 'login');
-$router->addRoute('GET', '/startup/login/forgot-password', StartupController::class, 'forgotPassword');
-$router->addRoute('GET', '/startup/login/forgot-password/reset-password', StartupController::class, 'resetPassword');
 
-$router->addRoute('GET', '/startup/signup', StartupController::class, 'signup');
-$router->addRoute('POST', '/startup/signup', StartupController::class, 'verification');
+$router->addRoute('GET', '/login', LoginController::class, 'index');
+
+$router->addRoute('GET', '/signup', SignupController::class, 'index');
 
 $router->addRoute('GET', '/ranking', RankingController::class, 'index');
 
 $router->addRoute('GET', '/exercises', ExercisesController::class, 'index');
 
 $router->addRoute('GET', '/training', TrainingController::class, 'index');
-$router->addRoute('GET', '/training/new', TrainingController::class, 'new');
-$router->addRoute('GET', '/training/start', TrainingController::class, 'new');
 
-$router->addRoute('GET', '/profile', TrainingController::class, 'new');
+
+$router->addRoute('GET', '/profile', ProfileController::class, 'index');
 
 $router->dispatch($_SERVER['REQUEST_METHOD'], parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
