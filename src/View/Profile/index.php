22ea800ca70dd05ef54b7cfec28375c36d3eabca
Remigiusz Nowicki <49556258+dotremilie@users.noreply.github.com>
2024-01-19 00:42:18 +0100

    Load data from array

--- src/View/Profile/index.php
+++ src/View/Profile/index.php
@@ -1,29 +1,32 @@
 <!doctype html>
 <html lang="en">
-<?php require_once __DIR__ . "/../../../templates/head.php"; ?>
+<?php use Wspomagacz\Model\ProfileStatistics;
+
+/** @var array $data */
+/** @var ProfileStatistics $profileStatistics */
+$profileStatistics = $data[0];
+
+require_once __DIR__ . "/../../../templates/head.php"; ?>
 <body class="bg-white dark:bg-slate-900 dark:text-white text-slate-800 mb-20">
 <?php require_once __DIR__ . "/../../../templates/header.php"; ?>
 <div class="flex flex-col justify-center items-center gap-4">
-    <div class="flex justify-between items-center h-32 dark:bg-slate-800 bg-slate-100 px-4">
-        <div>
-            <div class="text-4xl font-semibold">sneakydog</div>
-            <div class="text-2xl"><span class="text-sm">KG</span></div>
-        </div>
+    <div class="flex flex-col w-full justify-center dark:bg-slate-800 bg-slate-100 p-4 gap-6">
+            <div class="text-4xl font-semibold"><?= $profileStatistics->getUsername(); ?></div>
     </div>
-    <section class="mx-auto rounded-xl px-6 w-full">
-        <div class="text-2xl font-bold mb-4">Statystyki</div>
+    <section class="mx-auto rounded-xl px-4 w-full">
+        <div class="text-2xl font-bold mb-4">Statystyki ogólne</div>
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
             <div class="dark:bg-slate-800 bg-slate-100 p-6 rounded-xl">
                 <div class="text-2xl dark:text-sky-400 text-sky-600 flex gap-2 items-center mb-2"><i class="ti ti-weight"></i>Podniesiony ciężar</div>
-                <div class="text-xl font-semibold">1000 kg</div>
+                <div class="text-xl font-semibold"><?= $profileStatistics->getWeight(); ?>kg</div>
             </div>
             <div class="dark:bg-slate-800 bg-slate-100 p-6 rounded-xl">
                 <div class="text-2xl dark:text-lime-400 text-lime-600 flex gap-2 items-center mb-2"><i class="ti ti-barbell"></i>Wykonane ćwiczenia</div>
-                <div class="text-xl font-semibold">10</div>
+                <div class="text-xl font-semibold"><?= $profileStatistics->getTrainings(); ?></div>
             </div>
             <div class="dark:bg-slate-800 bg-slate-100 p-6 rounded-xl">
                 <div class="text-2xl dark:text-amber-400 text-amber-600 flex gap-2 items-center mb-2"><i class="ti ti-flame"></i>Spalone kalorie</div>
-                <div class="text-xl font-semibold text-red-400"><i class="ti ti-exclamation-circle"></i> Nie udało się obliczyć.</div>
+                <div class="text-xl font-semibold"><?= $profileStatistics->getBurntCalories(); ?></div>
             </div>
         </div>
     </section>
