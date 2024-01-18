d589228ba72fa203312a93f3830ac84143978d4f
Patryk Haufa <suprenoctome@gmail.com>
2024-01-18 20:45:27 +0100

    EXER

--- src/Model/CustomExercise.php
+++ src/Model/CustomExercise.php
@@ -4,10 +4,11 @@ namespace Wspomagacz\Model;
 
 class CustomExercise extends Exercise
 {
-    protected User $user;
+    protected int $user_id;
 
-    public function __construct(int $id, string $name, array $equipmentUsed, array $musclesUsed)
+    public function __construct(int $id, string $name, array $equipmentUsed, array $musclesUsed, int $user_id)
     {
         parent::__construct($id, $name, $equipmentUsed, $musclesUsed);
+        $this->user_id = $user_id;
     }
 }
