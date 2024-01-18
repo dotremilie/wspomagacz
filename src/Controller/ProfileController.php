1967acac134314aae18a74f2c059c7a430496966
Patryk Haufa <suprenoctome@gmail.com>
2024-01-18 23:43:34 +0100

    Backend ProfileController

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,15 +2,58 @@
 
 namespace Wspomagacz\Controller;
 
+use Wspomagacz\Core\Database;
+use Wspomagacz\Model\ProfileStatistics;
 use Wspomagacz\View\View;
 
 class ProfileController
 {
+    private ProfileStatistics $profileStatistics;
+
     public function index(): void
     {
         if (!isset($_SESSION['user_id'])) header('Location: /startup');
 
+        $this->fetchProfile($_SESSION['user_id']);
+
         $view = new View(__DIR__ . '/../View/Profile');
-        $view->render('index', [], 'Wspomagacz | Profil');
+        $view->render('index', [$this->getProfileStatistics()], 'Wspomagacz | Profil');
+    }
+
+    private function fetchProfile(int $user_id): void
+    {
+        $database = new Database();
+
+        $query = "
+        SELECT
+            u.id AS user_id,
+            u.username,
+            COUNT(DISTINCT t.id) AS training_count,
+            SUM(tes.repetitions * tes.weight) AS weight_sum
+        FROM
+            users u
+                LEFT JOIN
+            trainings t ON u.id = t.user_id
+                LEFT JOIN
+            training_exercises te ON t.id = te.training_id
+                LEFT JOIN
+            training_exercises_sets tes ON te.id = tes.training_exercise_id
+        WHERE
+            u.id = :user_id";
+
+        $data = $database->query($query, ["user_id"=>$user_id])->fetch();
+        $database->close();
+
+        $this->profileStatistics = new ProfileStatistics($data['user_id'], $data['username'], $data['training_count'], $data['weight_sum']);
+    }
+
+    public function getProfileStatistics(): ProfileStatistics
+    {
+        return $this->profileStatistics;
+    }
+
+    public function setProfileStatistics(ProfileStatistics $ProfileStatistics): void
+    {
+        $this->profileStatistics = $ProfileStatistics;
     }
 }
---
+++ src/Model/ProfileStatistics.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Wspomagacz\Model;
+
+class ProfileStatistics
+{
+    private int $id;
+    private string $username;
+    private int $trainings;
+    private int $weight;
+
+    public function __construct(int $id, string $username, int $trainings, int $weight)
+    {
+        $this->id = $id;
+        $this->username = $username;
+        $this->trainings = $trainings;
+        $this->weight = $weight;
+    }
+
+    public function getWeight(): int
+    {
+        return $this->weight;
+    }
+
+    public function getTrainings(): int
+    {
+        return $this->trainings;
+    }
+
+    public function getUsername(): string
+    {
+        return $this->username;
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+}
