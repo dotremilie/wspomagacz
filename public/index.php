379e2372fbcff236bc3075d9c04f14536f67941f
Remigiusz Nowicki <49556258+dotremilie@users.noreply.github.com>
2024-01-19 05:12:13 +0100

    add routes to handle trainings

--- public/index.php
+++ public/index.php
@@ -9,7 +9,6 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use Wspomagacz\Controller\ExercisesController;
 use Wspomagacz\Controller\HomeController;
 use Wspomagacz\Controller\LoginController;
-use Wspomagacz\Controller\ProfileController;
 use Wspomagacz\Controller\SignupController;
 use Wspomagacz\Controller\StartupController;
 use Wspomagacz\Controller\RankingController;
@@ -31,10 +30,13 @@ $router->addRoute('GET', '/ranking', RankingController::class, 'index');
 $router->addRoute('GET', '/exercises', ExercisesController::class, 'index');
 $router->addRoute('GET', '/exercises/{id}', ExercisesController::class, 'show');
 
-$router->addRoute('GET', '/training', TrainingController::class, 'index');
-$router->addRoute('GET', '/training/{id}', TrainingController::class, 'show');
+$router->addRoute('GET', '/trainings', TrainingController::class, 'index');
 
+$router->addRoute('GET', '/trainings', TrainingController::class, 'index');
+$router->addRoute('GET', '/trainings/create', TrainingController::class, 'index');
 
-$router->addRoute('GET', '/profile', ProfileController::class, 'index');
+$router->addRoute('GET', '/trainings/{id}', TrainingController::class, 'show');
+$router->addRoute('GET', '/trainings/{training_id}/exercises/{exercise_id}', TrainingController::class, 'show');
+$router->addRoute('GET', '/trainings/{training_id}/exercises/{exercise_id}/sets/{set_id}', TrainingController::class, 'show');
 
 $router->dispatch($_SERVER['REQUEST_METHOD'], parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
