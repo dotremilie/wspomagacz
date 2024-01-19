3ccd1572a24f5b6e23fb896464f23cd077105653
Remigiusz Nowicki <49556258+dotremilie@users.noreply.github.com>
2024-01-19 02:41:22 +0100

    Add support for parameters

--- src/Core/Router.php
+++ src/Core/Router.php
@@ -18,8 +18,8 @@ class Router {
     public function dispatch($method, $path): void
     {
         foreach ($this->routes as $route) {
-            if ($route['method'] === $method && $this->matchPath($route['path'], $path)) {
-                $this->callControllerAction($route['controller'], $route['action']);
+            if ($route['method'] === $method && ($params = $this->matchPath($route['path'], $path)) !== false) {
+                $this->callControllerAction($route['controller'], $route['action'], $params);
                 return;
             }
         }
@@ -28,14 +28,34 @@ class Router {
         echo '404 Not Found';
     }
 
-    private function matchPath($routePath, $requestPath): bool
+    private function matchPath($routePath, $requestPath): array|bool
     {
-        return rtrim($routePath, '/') === rtrim($requestPath, '/');
+        $routePathParts = explode('/', trim($routePath, '/'));
+        $requestPathParts = explode('/', trim($requestPath, '/'));
+
+        if (count($routePathParts) !== count($requestPathParts)) {
+            return false;
+        }
+
+        $params = [];
+
+        foreach ($routePathParts as $index => $routePart) {
+            if (str_starts_with($routePart, '{') && strrpos($routePart, '}') === strlen($routePart) - 1) {
+                // This part of the route is a parameter
+                $paramName = substr($routePart, 1, -1);
+                $paramValue = urldecode($requestPathParts[$index]);
+                $params[$paramName] = $paramValue;
+            } elseif ($routePart !== $requestPathParts[$index]) {
+                return false;
+            }
+        }
+
+        return $params;
     }
 
-    private function callControllerAction($controller, $action): void
+    private function callControllerAction($controller, $action, array $params): void
     {
         $controllerInstance = new $controller();
-        $controllerInstance->$action();
+        $controllerInstance->$action($params);
     }
 }
