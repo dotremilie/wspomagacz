a0658342cc552541c43b990c65620cba734c1439
Remigiusz Nowicki <49556258+dotremilie@users.noreply.github.com>
2024-01-19 05:11:20 +0100

    add bg for light layout

--- templates/header.php
+++ templates/header.php
@@ -1,4 +1,4 @@
-<nav class="dark:bg-slate-900 p-3 dark:text-slate-100 text-slate-800 sticky top-0">
+<nav class="bg-white dark:bg-slate-900 p-3 dark:text-slate-100 text-slate-800 sticky top-0">
     <div class="w-full text-center">
         <h1 class="text-3xl font-bold italic">WSPOMAGACZ</h1>
     </div>
