57cf275316812f2f069bbdd985c872ba1016c7da
Remigiusz Nowicki <49556258+dotremilie@users.noreply.github.com>
2024-01-19 05:09:38 +0100

    add name in TrainingExercise.php

--- src/Model/TrainingExercise.php
+++ src/Model/TrainingExercise.php
@@ -7,16 +7,18 @@ use Wspomagacz\Enums\TrainingStatus;
 class TrainingExercise
 {
     private int $id;
+    private string $name;
     private int $order;
     private TrainingStatus $status;
     private array $sets;
 
-    public function __construct(int $id, int $order, TrainingStatus $status, array $sets)
+    public function __construct(int $id, string $name, int $order, TrainingStatus $status, array $sets = [])
     {
         $this->id = $id;
         $this->order = $order;
         $this->sets = $sets;
         $this->status = $status;
+        $this->name = $name;
     }
 
     public function getOrder(): int
@@ -57,5 +59,15 @@ class TrainingExercise
     public function setStatus(TrainingStatus $status): void
     {
         $this->status = $status;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
     }
 }
