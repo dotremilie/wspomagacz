2a202e7f1db72def78611549770348f23aa900e7
Patryk Haufa <suprenoctome@gmail.com>
2024-01-19 19:55:24 +0100

    Update LoginController.php

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -2,6 +2,7 @@
 
 namespace Wspomagacz\Controller;
 
+use Wspomagacz\Core\Database;
 use Wspomagacz\Model\InputField;
 use Wspomagacz\View\View;
 
@@ -14,5 +15,17 @@ class LoginController
 
         $view = new View(__DIR__ . '/../View/Login');
         $view->render('index', [], 'Wspomagacz | Logowanie');
+    }
+
+    private function verifyLogin(string $username, string $password): bool
+    {
+        $database = new Database();
+
+        $query = "SELECT u.password_hash FROM users u WHERE u.username = :username;";
+
+        $password_hash = $database->query($query, ['username' => $username])->fetch();
+
+        if (password_verify($password, $password_hash)) return true;
+        return false;
     }
 }
