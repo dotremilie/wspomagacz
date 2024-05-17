f37005afdf4b29ef83cee732f591ba727046132b
Remigiusz Nowicki <49556258+dotremilie@users.noreply.github.com>
2024-05-17 14:59:13 +0200

    init

---
+++ Wspomagacz.MAUI/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Wspomagacz.MAUI;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Wspomagacz.MAUI/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Wspomagacz.MAUI;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
